2eff87b22c7beafa0d473d8352b700251c22f251
masatotezuka <tez.0731.tez@gmail.com>
2022-03-08 23:43:53 +0900

    DOM

--- index.js
+++ index.js
@@ -1,73 +1,23 @@
-const arr = new Array(1000000).fill(0).map((v, i) => i);
+const getId = (id) => {
+  const element = document.getElementById(id);
+  console.log(id);
+};
 
-let sum = 0;
-const len = arr.length | 0;
-function addSum(v) {
-  sum += v;
-}
+const getClasses = (className) => {
+  const classes = document.getElementsByClassName(className);
+  console.log(classes);
+};
 
-for (let j = 0; j < 5; j++) {
-  sum = 0;
-  console.time("for");
-  for (let i = 0; i < len; i++) {
-    sum += arr[i];
+const submitForm = () => {
+  // event.preventDefault();
+  const form = document.forms.demoForm;
+  const title = form.title.value;
+  const languages = [];
+  for (const language of form.languages) {
+    if (language.checked) {
+      languages.push(language.value);
+    }
   }
-  console.timeEnd("for");
-  console.log(sum);
-}
-
-for (let i = 0; i < 5; i++) {
-  sum = 0;
-  console.time("for of");
-  for (const v of arr) {
-    sum += v;
-  }
-  console.timeEnd("for of");
-  console.log(sum);
-}
-
-for (let i = 0; i < 5; i++) {
-  sum = 0;
-  console.time("forEach(arrow)");
-  arr.forEach((v) => {
-    sum += v;
-  });
-  console.timeEnd("forEach(arrow)");
-  console.log(sum);
-}
-
-for (let i = 0; i < 5; i++) {
-  sum = 0;
-  console.time("forEach(function)");
-  arr.forEach(addSum);
-  console.timeEnd("forEach(function)");
-  console.log(sum);
-}
-
-for (let i = 0; i < 5; i++) {
-  sum = 0;
-  console.time("map(arrow)");
-  arr.map((v) => {
-    sum += v;
-  });
-  console.timeEnd("map(arrow)");
-  console.log(sum);
-}
-
-for (let i = 0; i < 5; i++) {
-  sum = 0;
-  console.time("map(function)");
-  arr.map(addSum);
-  console.timeEnd("map(function)");
-  console.log(sum);
-}
-
-for (let j = 0; j < 5; j++) {
-  sum = 0;
-  console.time("Typed for");
-  for (let i = 0; i < len; i++) {
-    sum += arr[i];
-  }
-  console.timeEnd("Typed for");
-  console.log(sum);
-}
+  console.log(title);
+  console.log(languages);
+};
