6db638633e9f31eb88434cf75c799a96556c5b83
masatotezuka <tez.0731.tez@gmail.com>
2022-03-09 18:33:55 +0900

    dom2

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
-const getId = (id) => {
+const setInnerText = (id) => {
   const element = document.getElementById(id);
+  console.log(element.innerText);
   console.log(id);
 };
 
-const getClasses = (className) => {
-  const classes = document.getElementsByClassName(className);
-  console.log(classes);
+const setInnerHtml = (id) => {
+  const element = document.getElementById(id);
+  const literal = "書き換えられたよ。";
+  console.log(element.innerText);
+  element.innerHTML = `<p id='child'>HTMLが${literal}</p>`;
 };
 
-const submitForm = () => {
-  // event.preventDefault();
-  const form = document.forms.demoForm;
-  const title = form.title.value;
-  const languages = [];
-  for (const language of form.languages) {
-    if (language.checked) {
-      languages.push(language.value);
-    }
-  }
-  console.log(title);
-  console.log(languages);
+const replaceImageSrc = (id) => {
+  const element = document.getElementById(id);
+  element.setAttribute("src", "./小胸筋.JPG");
+};
+
+let counter = 0;
+
+const appendChildNode = (id) => {
+  const element = document.getElementById(id);
+  element.insertAdjacentHTML("beforeend", `<div>${counter}つ目の要素</div>`);
+  counter++;
 };
